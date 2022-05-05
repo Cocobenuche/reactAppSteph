5bcccd4268e93c73388d243e4fd80e781a4b10c1
Cocobenuche <104145119+Cocobenuche@users.noreply.github.com>
2022-05-05 17:59:37 +0200

    same

--- src/ListDepartments.js
+++ src/ListDepartments.js
@@ -1,3 +1,8 @@
 import React from "react";
 
-//export default ListDepartments;
+class ListDepartments extends React.Component {
+  render() {
+    return <div>Coucou</div>;
+  }
+}
+export default ListDepartments;
