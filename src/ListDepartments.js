e7724db1ddb72ba52f86f8fb6cd910b299f40a01
Cocobenuche <104145119+Cocobenuche@users.noreply.github.com>
2022-05-06 17:35:53 +0200

    fin

--- src/ListDepartments.js
+++ src/ListDepartments.js
@@ -1,9 +1,11 @@
-import React from "react";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 class ListDepartments extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+    this.state = { data: [], value: "", infos: false, dptInfos: [] };
   }
 
   componentDidMount() {
@@ -12,34 +14,72 @@ class ListDepartments extends React.Component {
       .then((dataDepartment) => this.setState({ data: dataDepartment }));
   }
 
+  handleChangeInputSearch(e) {
+    const value = e.target.value;
+    this.setState({ value });
+  }
+
+  handleClickInfos(dep) {
+    this.setState({ infos: true, dptInfos: dep });
+  }
+
+  handleClickRetour() {
+    this.setState({ infos: false, dtpInfos: "" });
+  }
+
   render() {
     if (this.state.data.length === 0) {
       return "Loading...";
     }
+
+    const lowerCaseValue = this.state.value.toLowerCase();
     const filteredDepartments = this.state.data.filter(
-      (dep) => dep.nom.indexOf(this.state.value) !== -1
+      (dep) => dep.nom.toLowerCase().indexOf(lowerCaseValue) !== -1
     );
-    const listDpts = filteredDepartments.map((departement) => (
+    const listDpts = filteredDepartments.map((dep) => (
       <li className="elementDeChaqueDpts">
-        <span className="noms">{departement.nom} </span>
-        <span className="codes">({departement.code})</span>
-        <span className="codeRegions">{departement.codeRegion}</span>
+        <span className="noms">{dep.nom} </span>
+        <span className="codes">({dep.code})</span>
+        <span className="codeRegions">{dep.codeRegion}</span>
+        <button
+          variant="outlined"
+          color="success"
+          className="dmdInfos"
+          onClick={() => this.handleClickInfos(dep)}
+        >
+          Plus d'informations sur le {dep.code}
+        </button>
       </li>
     ));
-    return (
-      <div className="Resultat">
-        <form>
-          <label>
-            Recherche :
-            <input type="text" name="name" value={this.state.data.nom} />
-          </label>
-        </form>
-        <div className="Retour">
-          <h4>Liste des départements</h4>
-          <div className="Lists">
-            <ul className="tout">{listDpts}</ul>
+    if (!this.state.infos) {
+      return (
+        <div className="Resultat">
+          <form>
+            <label>
+              Recherche :
+              <input
+                type="text"
+                value={this.state.data.nom}
+                onChange={(e) => this.handleChangeInputSearch(e)}
+              />
+            </label>
+          </form>
+          <div className="Retour">
+            <h4>Liste des départements</h4>
+            <div className="Lists">
+              <ul className="tout">{listDpts}</ul>
+            </div>
           </div>
         </div>
+      );
+    }
+    return (
+      <div>
+        Voici des informations sur le {this.state.dptInfos.nom}. Son code postal
+        est le {this.state.dptInfos.code}, et il est situé dans la région{" "}
+        {this.state.dptInfos.codeRegion}.
+        <br />
+        <button onClick={() => this.handleClickRetour()}>Retour</button>
       </div>
     );
   }
