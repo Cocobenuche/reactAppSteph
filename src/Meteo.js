e81ea0f2b1d9bb042de0e00990d1ab10a37c01fa
Cocobenuche <cobenat@icloud.com>
2022-05-13 16:56:36 +0200

    modifs meteo

--- src/Meteo.js
+++ src/Meteo.js
@@ -1,86 +1,93 @@
 import React, { Component } from "react";
 
-class MeteoNantes extends React.Component{
-    constructor(props){
-    super(props);
-    this.state={
-        items:[],
-        today:[],
-        inputSearchValue:"",
-        isSubmitted:false,
-        lien:"",
-        lienVille:"",
-        listeVilles:[]
+class MeteoNantes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            today: [],
+            inputSearchValue: "",
+            isSubmitted: false,
+            lien: "",
+            lienVille: "",
+            listeVilles: []
 
-    };
-    this.handleChangeInput = this.handleChangeInput.bind(this);
+        };
+        this.handleChangeInput = this.handleChangeInput.bind(this);
     }
-    
+
 
 
     handleChangeInput(e) {
         const { value } = e.target;
+        let url = "https://geo.api.gouv.fr/communes?nom=" + value + "&fields=departement&boost=population&limit=5";
         this.setState({
-          inputSearchValue: value,
-          lienVille:"https://geo.api.gouv.fr/communes?nom="+value+"&fields=departement&boost=population&limit=5",
+            inputSearchValue: value,
+            lienVille: url
         });
-        fetch(this.state.lienVille)
-        .then((data) => data.json())
-        .then((data) => {this.setState({listeVilles:data});});
+        console.log(this.state.lienVille);
+        //fetch(this.state.lienVille).then((data) => {console.log(data);
+           // data.json().then((data) => {this.setState({ listeVilles: data })})});
+           fetch(this.state.lienVille)
+           .then((data) => { console.log(data); data.json().then((data) => { console.log(data) }) });
+
     }
-    handleSubmit(){
+
+    handleSubmit() {
         this.setState({
-          isSubmitted: true
+            isSubmitted: true
         });
- 
-            fetch(this.state.lien)
+
+        fetch(this.state.lien)
             .then((data) => data.json())
             .then((data) => {
-            this.setState({
-                items: data.city_info,
-                today: data.current_condition
-            });
+                this.setState({
+                    items: data.city_info,
+                    today: data.current_condition
+                });
             });
     }
-    goBack(){
+    goBack() {
         this.setState({
-            isSubmitted : false,
-            lien:"",
-            items:[],
-            today:[],inputSearchValue:""
+            isSubmitted: false,
+            lien: "",
+            items: [],
+            today: [], inputSearchValue: ""
         })
     }
 
-    render(){
-        const {items, today,inputSearchValue,isSubmitted,lien}=this.state;
+    render() {
+        const { items, today, inputSearchValue, isSubmitted, lien } = this.state;
 
-        return(
-        <div>
-            <h1>Données météo</h1>
-            <label for="city">Choisissez une ville : </label>
+        return (
+            <div>
+                <h1>Données météo</h1>
+                <label for="city">Choisissez une ville : </label>
                 <input
-                id="city"
-                type="text"
-                name="nom"
-                onChange={this.handleChangeInput}            
+                    id="city"
+                    type="text"
+                    name="nom"
+                    onChange={this.handleChangeInput}
                 />
-                <li><button onClick={() =>this.handleSubmit()}>{this.state.listeVilles}</button></li>
-            
+                <li>
+                    <button onClick={() => this.handleSubmit()}>{this.state.listeVilles}</button>
+                </li>
 
-         {isSubmitted &&(
-         <>
-         <div className="resultats">
-         <h1>{items.name} :</h1>
-         <h5>Le soleil se lève à {items.sunrise} et se couche à {items.sunset}.</h5>
-         <h6>Il fait actuellement {today.tmp} degrés, et le ciel est {today.condition}.</h6>
-         <button onclick={() => this.goBack()}>Retour</button>
-         </div>
-         </>)
-         
-         }
-     
-   </div>
-    )    
-}
+
+                {isSubmitted && (
+                    <>
+                        <div className="resultats">
+                            <h1>{items.name} :</h1>
+                            <h5>Le soleil se lève à {items.sunrise} et se couche à {items.sunset}.</h5>
+                            <h6>Il fait actuellement {today.tmp} degrés, et le ciel est {today.condition}.</h6>
+                            <button onclick={() => this.goBack()}>Retour</button>
+                        </div>
+                    </>)
+
+                }
+
+            </div>
+        )
+    }
 }
 export default MeteoNantes;
